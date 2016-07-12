d8503d537ec91aed7d3c531a847eeadf1ed25907
Cambo <cameroncarroll.cam@gmail.com>
2016-07-12 16:15:27 +0100

    Php Form Test

--- Bugs.php
+++ Bugs.php
@@ -1,25 +1,12 @@
 <?php
-define('DB_NAME', '1301070');
-define('DB_USER', 'b05411072e2e07');
-define('DB_PASSWORD','2e5e5133');
-define('DB_HOST', 'eu-cdbr-azure-west-d.cloudapp.net');
+$db = newmsqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
 
-$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
-
-if (!$link){
-    die('Could not connect: ' . mysql_error());
+//test connection
+if($db->connect_errno){
+    die('connection failed : '.db->connect_error );
 }
-
-$db_selected = mysql_select_db(DB_NAME,$link);
-
-if(!$db_selected){
-    die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
-}
-echo 'Connected successfully';
-
-/**
- * Created by PhpStorm.
- * User: Cambo
- * Date: 07/07/2016
- * Time: 16:00
- */
