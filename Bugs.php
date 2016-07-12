50c7cb12db63f11ebd6fa2c49ba7d1bf55d14628
Cambo <cameroncarroll.cam@gmail.com>
2016-07-12 15:09:10 +0100

    Php Form Test

--- Bugs.php
+++ Bugs.php
@@ -1,4 +1,21 @@
 <?php
+define('DB_NAME', '1301070');
+define('DB_USER', 'b05411072e2e07');
+define('DB_PASSWORD','2e5e5133');
+define('DB_HOST', 'eu-cdbr-azure-west-d.cloudapp.net');
+
+$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
+
+if (!$link){
+    die('Could not connect: ' . mysql_error());
+}
+
+$db_selected = mysql_select_db(DB_NAME,$link);
+
+if(!$db_selected){
+    die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+}
+echo 'Connected successfully';
 
 /**
  * Created by PhpStorm.
@@ -6,7 +23,3 @@
  * Date: 07/07/2016
  * Time: 16:00
  */
-class Bugs
-{
-    
-}
