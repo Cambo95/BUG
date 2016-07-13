811c8d20460399e49f227e1803e0b4d006242b98
Cambo <cameroncarroll.cam@gmail.com>
2016-07-13 13:06:03 +0100

    Comment Php link to database

--- Bugs.php
+++ Bugs.php
@@ -1,25 +1,25 @@
 <?php
 
-echo " Start";
+//echo " Start";
 $db = new mysqli(
     "eu-cdbr-azure-west-d.cloudapp.net",
     "b05411072e2e07",
     "2e5e5133",
     "1301070"
 );
-echo " About to do Test Connection";
+//echo " About to do Test Connection";
 //test connection
 if($db->connect_errno){
     die('connection failed : '.$db->connect_error );
 
 }
-else echo "Test Connection Successful";
+//else echo "Test Connection Successful";
 
 $sql_query = "SELECT com_comment FROM  bug_comments ";
 // execute the SQL query
 $result = $db->query($sql_query);
 
-echo "Query Ran 2";
+//echo "Query Ran 2";
 
 if ($result->num_rows > 0) {
     // output data of each row
