00a03e4f911050030ccc4173d1c1821d04d1c53f
Cambo <cameroncarroll.cam@gmail.com>
2016-07-15 15:42:20 +0100

    Footer1

---
+++ Development/CommonFooter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>SPLAT! Bug Catcher</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<body>
+
+<footer class="w3-container w3-teal">
+    <h5>Footer</h5>
+    <p>Footer information goes here</p>
+</footer>
+
+</body>
+</html>
