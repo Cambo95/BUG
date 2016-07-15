439cfc748da9ea2fcea2f151dd808c1e58a84799
Cambo <cameroncarroll.cam@gmail.com>
2016-07-15 15:30:10 +0100

    Common Login

---
+++ Development/CommonLogin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>SPLAT! Bug Catcher</title>
+<meta charset="UTF-8">
+
+<body>
+<form>
+    Username:<br>
+    <input type ="text" name="username">
+    <br>
+    Password:<br>
+    <input type="password" name="password">
+    <br>
+    <input type ="submit" value ="Login">
+</form>
+</body>
+</html>
