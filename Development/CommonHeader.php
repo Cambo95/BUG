c2aaad57a7422a11efbba103858dd0e1b1509598
Cambo <cameroncarroll.cam@gmail.com>
2016-07-15 14:52:11 +0100

    CommonHeader9

--- Development/CommonHeader.php
+++ Development/CommonHeader.php
@@ -3,27 +3,6 @@
 <title>SPLAT! Bug Catcher</title>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<head>
-    <style>
-        input[type=text]{
-            width: 130px;
-            box-sizing: border-box;
-            border: 2px solid #ccc;
-            border-radius: 4px;
-            font-size: 16px;
-            background-color: white;
-            background-image: url('https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-128.png');
-            background-position: 10px 10px;
-            background-repeat: no-repeat;
-            padding: 12px 20px 12px 40px;
-            -webkit-transition: width 0.4s ease-in-out;
-            transition: width 0.4s ease-in-out;
-        }
-        input[type=text]:focus{
-            width:100%;
-        }
-    </style>
-</head>
 <body>
     <header class="w3-container w3-teal">
         <H1> SPLAT! Bug Catcher</H1>
