6dcd7b4eb2ff1fe253e7acee293310af5443f8d3
Cambo <cameroncarroll.cam@gmail.com>
2016-07-15 15:57:23 +0100

    Homepage1

---
+++ Development/HomePage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cambo
+ * Date: 13/07/2016
+ * Time: 16:25
+ */
+
+/** Setup Database Connection */
+/** ======================================================================= */
+$db = new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
+
+/?>
+
+<html>
+<head>
+    <title>SPLAT! Bug Catcher</title>
+</head>
+<body>
+    <?php include 'CommonHeader.php';?>
+    <?php include 'CommonLogin.php';?>
+        <ul>
+            <li><a href="http://1301070cameronbug.azurewebsites.net/production/useralbugs.php">Register (Page Not linked yet)</a></li>
+        </ul>
+    <?php include 'CommonFooter.php';?>
+</body>
