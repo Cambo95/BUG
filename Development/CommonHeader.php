1ab8daa480c587131b6b3f0953154b7475362577
Cambo <cameroncarroll.cam@gmail.com>
2016-07-15 15:21:31 +0100

    Common Header and Footer

--- Development/CommonHeader.php
+++ Development/CommonHeader.php
@@ -24,5 +24,11 @@
 <form>
     <input type="text" name="search" placeholder="Search...">
 </form>
+
+    <footer class="w3-container w3-teal">
+        <h5>Footer</h5>
+        <p>Footer information goes here</p>
+    </footer>
+
 </body>
 </html>
