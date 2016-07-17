cb64692085cba36e1e730c306ebd36d9f8f1e926
Cambo <cameroncarroll.cam@gmail.com>
2016-07-17 21:02:58 +0100

    Registration

---
+++ Development/Registration.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cambo
+ * Date: 13/07/2016
+ * Time: 16:25
+ */
+
+$db = new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <title>SPLAT! Bug Catcher</title>
+    <meta charset="UTF-8">
+    <head>
+        <title>SPLAT! Bug Catcher</title>
+    </head>
+    <body>
+<?php include 'CommonHeader.php';?>
+        <form>
+            Name:<br>
+            <input type="text" name="Name">
+            <br>
+            Surname:<br>
+            <input type="text" name="Surname">
+            <br>
+            Country:<br>
+            <input type="text" name="Country">
+            <br>
+            Bio:<br>
+            <input type="text" name="Bio" maxlength="1000">
+        </form>
+    </body>
+</html>
