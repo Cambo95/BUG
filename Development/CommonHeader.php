825cb28ad2fac145ea5394704d58f6a21bcbd25e
Cambo <cameroncarroll.cam@gmail.com>
2016-07-19 12:46:18 +0100

    Home button again

--- Development/CommonHeader.php
+++ Development/CommonHeader.php
@@ -6,7 +6,6 @@
 <body>
     <header class="w3-container w3-teal">
         <H1> SPLAT! Bug Catcher</H1>
-        <H1> SPLAT! Bug Catcher</H1>
         <ul>
             <li><a href="http://1301070cameronbug.azurewebsites.net/development/homepage.php">Home</a></li>
         </ul>
