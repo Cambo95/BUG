7a4c2c3233eac3e2ec99776a6a14c958eb8d4ef3
Cambo <cameroncarroll.cam@gmail.com>
2016-07-19 14:00:55 +0100

    Upload attachment

--- Development/BugWithComments.php
+++ Development/BugWithComments.php
@@ -78,6 +78,13 @@ while($Date = mysqli_fetch_assoc($resultDate)) {
     }//end while
     ?>
 </table>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
 <br>
 <form method = 'post'>
 Comment:<br />
