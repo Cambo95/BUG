f15a275747629bc55da83009abb63beeda168498
Cambo <cameroncarroll.cam@gmail.com>
2016-07-20 19:53:14 +0100

    Login

--- Development/CommonLogin.php
+++ Development/CommonLogin.php
@@ -12,10 +12,6 @@ $db = new mysqli(
     "2e5e5133",
     "1301070"
 );
-session_start();
-$_SESSION['Username']="CAM";
-
-print $_SESSION['Username'];
 
 ?>
 
@@ -30,9 +26,37 @@ print $_SESSION['Username'];
     <label>Password</label><input type=“password” name=“Password”>
     <input type="submit" name="submit">
 </form>
-<?php 
-    if(isset($_POST['submit'])){
-    echo "Cam pressed Login";
+<?php
+session_start();
+
+$user_check =$_SESSION['login_user'];
+$ses_sql = mysqli_query($db,"SELECT Usr_User FROM bug_userprofile WHERE Usr_User ='$user_check'");
+$row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+$login_session = $row['Usr_User'];
+if(!isset($_SESSION['login_user'])){
+    header("location: homepage.php");
+}
+
+$user=$_REQUEST['Username'];
+$pass=$_REQUEST['Password'];
+
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $username=mysqli_real_escape_string($db,$_POST['Username']);
+        $password=mysqli_real_escape_string($db,$_POST["Password"]);
+
+        $sql = "SELECT Usr_User FROM bug_userprofile WHERE Usr_User = '$username' and Usr_Password ='$password'";
+        $result = mysqli_query($db,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $active = $row['active'];
+
+        $count = mysqli_num_rows($result);
+
+        if($count == 1){
+            $SESSION['login_user']= $username;
+            header("location: bug_userprofile_display.php");
+        }else{
+            $error = "Your Login Name or Password is invalid";
+        }
 }
 ?>
 </body>
