9f49356617cc618e809ec48b6fb26d7810c8a2c9
Cambo <cameroncarroll.cam@gmail.com>
2016-07-20 13:31:58 +0100

    Session

--- Development/CommonLogin.php
+++ Development/CommonLogin.php
@@ -1,8 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cambo
+ * Date: 13/07/2016
+ * Time: 16:25
+ */
+
+$db = new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <title>SPLAT! Bug Catcher</title>
 <meta charset="UTF-8">
-
+<?php
+session_start();
+$Session['Usr_User']=$username;
+echo $Session['Usr_User'];
+?>
 <body>
 <form>
     Username:<br>
