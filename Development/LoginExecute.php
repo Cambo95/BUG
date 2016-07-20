9711dc8b5a1d121172038b74aa67a756ec517d03
Cambo <cameroncarroll.cam@gmail.com>
2016-07-20 16:49:33 +0100

    Login Execute 3

--- Development/LoginExecute.php
+++ Development/LoginExecute.php
@@ -1,4 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>SPLAT! Bug Catcher</title>
+<meta charset="UTF-8">
+<body>
+
 <?php
 if(isset($_POST['Login'])){
-    echo("You clicked Login"); 
-    ?>
+echo("You clicked Login");
+?>
+
+</body>
+</html>
