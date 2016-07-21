de0a11afa9a0a272af1cd3b17a6048da0e5b8ddc
Cambo <cameroncarroll.cam@gmail.com>
2016-07-21 17:23:32 +0100

    Check Login database

---
+++ Development/Checklogindb.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>SPLAT! Bug Catcher</title>
+<meta charset="UTF-8">
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cambo
+ * Date: 13/07/2016
+ * Time: 16:25
+ */
+
+$db = new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
+$username = $_POST['username'];
+$password = $_POST['password'];
+echo $username;
+echo $password;
+?>
+
+<?php
+
+
+    $username = mysqli_real_escape_string($db,$_POST['username']);
+    $password = mysqli_real_escape_string($db,$_POST['password']);
+
+    $sql = "SELECT * FROM bug_userprofile WHERE Usr_User = '$username' and Usr_Password = '$password'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
+
+    $count = mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row
+
+    if($count == 1) {
+        echo 'Boob';
+    }else {
+        $error = "Your Login Name or Password is invalid";
+    }
+?>
+</body>
+</html>
