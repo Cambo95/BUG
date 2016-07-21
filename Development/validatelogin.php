d8fa3032c7340829825a17e8d6b42a77d84d1ed8
Cambo <cameroncarroll.cam@gmail.com>
2016-07-21 16:03:41 +0100

    Login Message 2

--- Development/validatelogin.php
+++ Development/validatelogin.php
@@ -5,8 +5,9 @@
 <body>
 
 Welcome <?
+$_POST = 'username';
 if(empty($_POST)){
-    echo'$_POST was not entered. Please try again';
+    echo'$_POST is empty';
     }else{
         echo $_POST['username'];
     }
