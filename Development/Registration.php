84c6a15170f84484f1ec98dd7c8ed30e2d3f5efc
Cambo <cameroncarroll.cam@gmail.com>
2016-07-24 17:49:41 +0100

    Registration Attempt 21

--- Development/Registration.php
+++ Development/Registration.php
@@ -31,58 +31,28 @@
         </form>
 
 <?php
+$servername =  "eu-cdbr-azure-west-d.cloudapp.net";
+$username = "b05411072e2e07";
+$password = "2e5e5133";
+$dbname = "1301070";
 
-
-$db = new mysqli(
-    "eu-cdbr-azure-west-d.cloudapp.net",
-    "b05411072e2e07",
-    "2e5e5133",
-    "1301070"
-);
-$con=mysqli_connect($db);
-
- function NewUser(){
-    echo 'In new user function';
-     $name = $_POST['Name'];
-     $surname = $_POST['Surname'];
-     $country = $_POST['Country'];
-     $bio = $_POST['Bio'];
-     $password = $_POST['Password'];
-     echo $name;
-     echo $surname;
-     echo $country;
-     echo $bio;
-     echo $password;
-     $query = "INSERT INTO bug_userprofile(Usr_User, Usr_Surname, Usr_Country, Usr_Bio, Usr_Password) VALUES ('$name','$surname','$country','$bio','$password')";
-     $data = mysqli_query($query);
-     if($data)
-     {
-         echo "YOUR REGISTRATION IS COMPLETED";
-     }
-     else {echo 'SQL FAILED';}
- }
-
-function SignUp()
-{
-    echo 'In signup function';
-    if (!empty($_POST['Name'])) {
-        $query = mysqli_query("SELECT * FROM bug_userprofile WHERE Usr_User = '$_POST[Name]'AND Usr_Password = '$_POST[Password]'");
-
-        if (!$row = mysqli_fetch_array($query)) {
-            echo 'Test says im a new user';
-            NewUser();
-        } else {
-            echo "SORRY, YOU ARE ALREADY A REGISTERED USER";
-        }
-    }
-}
-        if(isset($_POST['submit'])){
-            echo 'About to execute signup';
-            SignUp();
-    
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "INSERT INTO bug_userprofile(Usr_User, Usr_Surname, Usr_Country, Usr_Bio, Usr_Password)
+VALUES('Mister','Test','Testland','TestTestTest','TestPassword')";
+
+if($conn->query($sql)=== TRUE){
+    echo "New record created successfully";
+}
+else{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
 ?>
+
 <ul>
     <li><a href="http://1301070cameronbug.azurewebsites.net/development/homepage.php">Cancel</a></li>
 </ul>
