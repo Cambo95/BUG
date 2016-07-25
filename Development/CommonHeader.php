a1d73c7f0b46d28976d3fc099afa2fbbe09c6ecf
Cambo <cameroncarroll.cam@gmail.com>
2016-07-25 17:55:36 +0100

    Login display name 13

--- Development/CommonHeader.php
+++ Development/CommonHeader.php
@@ -2,9 +2,7 @@
 <li class='active' style='float:right; color: white; padding: 14px 20px; margin 8px 0; border: none;'>
     <?php
     if ($_SESSION["username"]== ''){
-        echo $_SESSION["username"];
-        echo '<a href="HomePage.php"><span>Not Signed in</span></a></li>';
-    }
+            }
     else{
         echo '<a href="Logout.php"><span>Logout</span></a></li>';  
     }
