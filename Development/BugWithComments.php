cdf6439c3bb2fb9c4a9817748bb7369360f4eb58
Cambo <cameroncarroll.cam@gmail.com>
2016-07-25 21:31:58 +0100

    Bug With Comments ID 8

--- Development/BugWithComments.php
+++ Development/BugWithComments.php
@@ -122,13 +122,12 @@ if(isset($_POST['submit'])) {
     if ($Comment == ""){
         echo "Please insert a comment";
     } else {
-        echo "bug id = ";
-        echo $bugid;
         $sql = "INSERT INTO bug_comments(Com_BugUniqueID, Com_User, Com_Comment)
 VALUES('$bugid','$UserLoggedOn','$Comment')";
 
         if (mysqli_query($conn, $sql)) {
             echo "Comment added successfully";
+            $Comment = "";
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
