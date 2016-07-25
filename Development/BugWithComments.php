2d86048f5115bf166b3ce7b13ae570cd6ba33575
Cambo <cameroncarroll.cam@gmail.com>
2016-07-25 21:24:25 +0100

    Add comment 6

--- Development/BugWithComments.php
+++ Development/BugWithComments.php
@@ -120,8 +120,10 @@ if(isset($_POST['submit'])) {
     if ($Comment == ""){
         echo "Please insert a comment";
     } else {
+        echo "bug id = ";
+        echo $bugid;
         $sql = "INSERT INTO bug_comments(Com_BugUniqueID, Com_User, Com_Comment)
-VALUES('$bugid','$UserLoggedOn''$Comment')";
+VALUES('$bugid','$UserLoggedOn','$Comment')";
 
         if (mysqli_query($conn, $sql)) {
             echo "Comment added successfully";
