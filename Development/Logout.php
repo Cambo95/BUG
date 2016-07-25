a9a92f88e7abcc37102af321a88de3f0f41c23da
Cambo <cameroncarroll.cam@gmail.com>
2016-07-25 17:41:46 +0100

    Logout.php

---
+++ Development/Logout.php
@@ -0,0 +1,20 @@
+<?php
+session_destroy();
+echo "Successfully logged out. Click HOME to return to the Homepage"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<title>SPLAT! Bug Catcher</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<body>
+<header class="w3-container w3-teal">
+    <H1> SPLAT! Bug Catcher</H1>
+    <ul>
+        <li><a href="http://1301070cameronbug.azurewebsites.net/development/homepage.php">Home</a></li>
+    </ul>
+</header>    
+</body>
+</html>
+
