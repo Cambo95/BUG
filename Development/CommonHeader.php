502987e7524b4f6f6ed1cf6e0c0a9cf186f8958b
Cambo <cameroncarroll.cam@gmail.com>
2016-07-26 17:48:32 +0100

    Header Update 6

--- Development/CommonHeader.php
+++ Development/CommonHeader.php
@@ -8,17 +8,13 @@
     <body>
     <header class="w3-container w3-teal">
         <H1> SPLAT! Bug Catcher</H1>
-        <ul>
-            <li><a href="http://1301070cameronbug.azurewebsites.net/development/homepage.php">Home</a></li>
-        </ul>
-        <ul>
-            <li><a href="http://1301070cameronbug.azurewebsites.net/development/registration.php">Register</a></li>
-        </ul>
     </header>
     </body>
     </html>
 
 <?php
+    echo '<a href="HomePage.php"><span>Home<br></span></a></li>';
+    echo '<a href="Registration.php"><span>Register<br></span></a></li>';
 if ($_SESSION["isadmin"]== '1'){
     echo '<a href="AdminPage.php"><span>Administrative Page<br></span></a></li>';
 }
