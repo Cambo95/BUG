f485c936ebc029e8c81c2cf3bc17fb7bb44864bd
Cambo <cameroncarroll.cam@gmail.com>
2016-07-26 14:07:55 +0100

    Common Header Update

--- Development/CommonHeader.php
+++ Development/CommonHeader.php
@@ -37,3 +37,18 @@
     </header>
     </body>
     </html>
+    <li class='active' style='float:right; color: white; border: none;'>
+<?php
+if ($_SESSION["username"]== ''){
+    echo '<a href="CommonLogin.php"><span>Not Signed in</span></a></li>';
+}
+else{
+    echo '<a href="Logout.php"><span>Logout</span></a></li>';
+}
+if ($_SESSION["isadmin"]== '1'){
+    echo '<a href="AdminPage.php"><span>Administrative Page</span></a></li>';
+}
+if ($_SESSION["isverified"]== '1'){
+    echo '<a href="CreateBug.php"><span>Add a bug</span></a></li>';
+}
+?>
