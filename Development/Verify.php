123f437a4137978a450cbe26515eef9c1759e5e8
Cambo <cameroncarroll.cam@gmail.com>
2016-07-27 18:10:35 +0100

    Verifying Users 12

--- Development/Verify.php
+++ Development/Verify.php
@@ -35,10 +35,10 @@ echo $verifyusername;
 </form>
 
 <?php
-
+$testsql ="UPDATE bug_userprofile SET Usr_IsVerified= 1 WHERE Usr_User= '.$verifyusername. '";
+echo $testsql;
 if(isset($_POST['submit'])) {
 $UserIsAdmin =  $_SESSION['isadmin'];
-
 if ($UserIsAdmin !== "1"){
 echo "You are not an admin. You do not have permission to verify accounts.";}
     else{
