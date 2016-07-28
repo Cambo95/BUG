25d09f6b9aa9527ba33211e431b2b4f2971c554e
Cambo <cameroncarroll.cam@gmail.com>
2016-07-28 20:54:48 +0100

    Deletion of comment 2

--- Development/DeleteSingleComments.php
+++ Development/DeleteSingleComments.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+$db = new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +15,7 @@ session_start();
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <body>
 <?php include 'CommonHeader.php';?>
+<br><br><br><br>
 
 <?php if($UserLoggedOn!==''): ?>
     <form action='' method="POST">
@@ -16,6 +23,7 @@ session_start();
     </form>
 <?php endif; ?>
 
+<br><br><br><br>
 <?php include 'CommonFooter.php';?>
 </body>
 </html>
