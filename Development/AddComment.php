2cafa3abc48556c5c40e170afb8a024bebf50e49
Cambo <cameroncarroll.cam@gmail.com>
2016-08-02 20:21:18 +0100

    comm link 12

--- Development/AddComment.php
+++ Development/AddComment.php
@@ -49,7 +49,7 @@ $UserLoggedOn = $_SESSION["username"];
 if($UserLoggedOn!==''): ?>
     <form action='' method="POST">
         Add comment:<br>
-        <textarea name="comment" id = "comment"></textarea>
+        <textarea name="Comment" id = "Comment"></textarea>
         <br>
         <br>
         <input type="submit" name="submit">
