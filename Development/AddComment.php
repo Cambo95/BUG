e6298a2e08457b344f88b1f08a66ded6d641387b
Cambo <cameroncarroll.cam@gmail.com>
2016-08-02 20:12:57 +0100

    comm link 8

--- Development/AddComment.php
+++ Development/AddComment.php
@@ -49,7 +49,7 @@ $UserLoggedOn = $_SESSION["username"];
 if($UserLoggedOn!==''): ?>
     <form action='' method="POST">
         Add comment:<br>
-        <input type="text" name="Comment" <input style ="height:200px;width:150px">>
+        <input type="text" name="Comment" <input style ="height:200px;width:600px">
         <br>
         <br>
         <input type="submit" name="submit">
