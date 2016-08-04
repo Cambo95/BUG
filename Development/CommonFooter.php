abede441352eb56ca058702a7c4c4e737bac9f5b
Cambo <cameroncarroll.cam@gmail.com>
2016-08-04 19:26:49 +0100

    Github to azure failing?

--- Development/CommonFooter.php
+++ Development/CommonFooter.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <body>
-<!-- Places a simple footer-->
+<!-- Places a simple footer--->
 <footer class="w3-container w3-teal">
     <p>Website by Cameron Carroll</p>
 </footer>
