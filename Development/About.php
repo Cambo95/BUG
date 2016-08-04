dc30222236d6cca11c3169529c3cd67a89e9db2d
Cambo <cameroncarroll.cam@gmail.com>
2016-08-04 16:55:02 +0100

    About 1

---
+++ Development/About.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+$UserLoggedOn = $_SESSION["username"];
+$bugid=$_GET["bugid"];
+$comuser = $_GET["comuser"];
+$comdatetime = $_GET["comdatetime"];
+$db = new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b05411072e2e07",
+    "2e5e5133",
+    "1301070"
+);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<title>SPLAT! Bug Catcher</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<body>
+<?php include 'CommonHeader.php';?>
+<br><br><br><br>
+
+<h3>Site statistics</h3>
+<br>
+<br>
+
+
+<?php
+        $NumberVerified = 0;
+        $sql = "SELECT COUNT(*) as $NumberVerified from bug_userprofile where Usr_IsVerified == 1";
+        $result = mysqli_query($db, $deletesql);
+        echo "Number of verified users: ",$NumberVerified;
+
+
+
+?>
+<br><br><br><br>
+<?php include 'CommonFooter.php';?>
+</body>
+</html>
