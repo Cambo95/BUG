9d51918952fd629b34e0b6c6a6ccb487ee798f64
Cambo <cameroncarroll.cam@gmail.com>
2016-08-04 15:58:00 +0100

    Revision 3

---
+++ Revision.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+function do_something(&$arg){
+    $retval = $arg;
+    $arg = $arg+1;
+    return $retval;
+}
+$a = 3;
+$b = do_something($a);
+echo $a," ",$b;
+?>
+</body>
+</html>
